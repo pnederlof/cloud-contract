2fe3f3eba255d26beb5e2d32d4bd0067068394ad
Peter <peter.nederlof@gmail.com>
2019-05-14 08:13:46 +0200

    feat: add spring initializer projects

---
+++ cloud-contract-consumer/src/main/java/nl/recurrent/cloudcontractconsumer/CloudContractConsumerApplication.java
@@ -0,0 +1,13 @@
+package nl.recurrent.cloudcontractconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudContractConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudContractConsumerApplication.class, args);
+	}
+
+}
---
+++ cloud-contract-producer/src/main/java/nl/recurrent/cloudcontractproducer/CloudContractProducerApplication.java
@@ -0,0 +1,13 @@
+package nl.recurrent.cloudcontractproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudContractProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudContractProducerApplication.class, args);
+	}
+
+}
